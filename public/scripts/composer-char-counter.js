52263b7e17d1cdf2e959fa2ea9d5c2499bb0d889
Victoria Wong <victoria.ys.wong@gmail.com>
2023-12-05 10:38:36 -0500

    added if chars is less than 0, turn text red for the counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   // --- our code goes here ---
   console.log("HELLLOOO");
 
-  $("#new-tweet-form").on('keypress', "textarea", function () {
+  $("#new-tweet-form").on('keydown', "textarea", function () {
     console.log(this); //The this keyword is a reference to the button
 
     const charCount = $(this).val().length;
@@ -10,6 +10,13 @@ $(document).ready(function () {
 
     const counter = $(this).parent().children().children('.counter');
     counter.text(charsLeft);
+
+    if (charsLeft < 0) {
+      counter.css("color", "#f13a1a");
+    } else {
+      counter.css("color", "#545149");
+    }
+
 
   });
 
